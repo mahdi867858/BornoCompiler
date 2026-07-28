ae9fc79996281765bf704724581e03d9e806a297
Mumshad Chowdhury Mahdi <mumshadchowdhurymahdi@gmail.com>
2026-07-28 14:29:39 +0600

    Update Main.java to run next step test case

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,27 @@
 package main;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import lexer.Lexer;
 import token.Token;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] files = {
-            "examples/test1.brn",
-            "examples/test2.brn",
-            "examples/test3.brn",
-            "examples/test4.brn"
-        };
 
-        for (String file : files) {
-            System.out.println("--- Running " + file + " ---");
-            try {
-                String source = Files.readString(Path.of(file));
-                Lexer lexer = new Lexer(source);
-                for (Token token : lexer.tokenize()) {
-                    System.out.println(token);
-                }
-            } catch (IOException e) {
-                System.err.println("Error reading file " + file + ": " + e.getMessage());
-            }
-            System.out.println();
+        String code = """
+        সংখ্যা বয়স = 20;
+
+        যদি (বয়স >= 18)
+        {
+            দেখাও("Adult");
         }
+        """;
+
+        Lexer lexer = new Lexer(code);
+
+        for (Token token : lexer.tokenize()) {
+            System.out.println(token);
+        }
+
     }
 
 }
