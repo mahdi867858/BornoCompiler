ec8c6110bc7a3de2bce607b860e6cd5c522f80b9
Tasmina <jumanachy7@gmail.com>
2026-07-28 14:25:43 +0600

    Implemented character-based lexer with keyword, number, string, and operators support

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -38,7 +38,156 @@ public class Lexer {
         }
     }
 
+    private Token readNumber() {
+        StringBuilder number = new StringBuilder();
+        while (currentChar != '\0' && Character.isDigit(currentChar)) {
+            number.append(currentChar);
+            advance();
+        }
+        return new Token(TokenType.NUMBER, number.toString());
+    }
+
+    private boolean isIdentifierChar(char c) {
+        return Character.isLetter(c) || Character.isDigit(c) || c == '_' || (c >= '\u0980' && c <= '\u09FF');
+    }
+
+    private Token readIdentifierOrKeyword() {
+        StringBuilder word = new StringBuilder();
+        while (currentChar != '\0' && isIdentifierChar(currentChar)) {
+            word.append(currentChar);
+            advance();
+        }
+        String value = word.toString();
+        switch (value) {
+            case "সংখ্যা":
+            case "লেখা":
+            case "যদি":
+            case "নাহলে":
+            case "যতক্ষণ":
+            case "দেখাও":
+                return new Token(TokenType.KEYWORD, value);
+            default:
+                return new Token(TokenType.IDENTIFIER, value);
+        }
+    }
+
+    private Token readString() {
+        advance(); // skip opening quote
+        StringBuilder str = new StringBuilder();
+        while (currentChar != '\0' && currentChar != '"') {
+            str.append(currentChar);
+            advance();
+        }
+        advance(); // skip closing quote
+        return new Token(TokenType.STRING, str.toString());
+    }
+
+    public Token nextToken() {
+        skipWhitespace();
+
+        if (currentChar == '\0') {
+            return new Token(TokenType.EOF, "");
+        }
+
+        if (Character.isDigit(currentChar)) {
+            return readNumber();
+        }
+
+        if (isIdentifierChar(currentChar)) {
+            return readIdentifierOrKeyword();
+        }
+
+        if (currentChar == '"') {
+            return readString();
+        }
+
+        switch (currentChar) {
+            case '(':
+                advance();
+                return new Token(TokenType.LEFT_PAREN, "(");
+
+            case ')':
+                advance();
+                return new Token(TokenType.RIGHT_PAREN, ")");
+
+            case '{':
+                advance();
+                return new Token(TokenType.LEFT_BRACE, "{");
+
+            case '}':
+                advance();
+                return new Token(TokenType.RIGHT_BRACE, "}");
+
+            case '=':
+                advance();
+                if (currentChar == '=') {
+                    advance();
+                    return new Token(TokenType.EQUAL, "==");
+                }
+                return new Token(TokenType.ASSIGN, "=");
+
+            case '!':
+                advance();
+                if (currentChar == '=') {
+                    advance();
+                    return new Token(TokenType.NOT_EQUAL, "!=");
+                }
+                return new Token(TokenType.UNKNOWN, "!");
+
+            case '<':
+                advance();
+                if (currentChar == '=') {
+                    advance();
+                    return new Token(TokenType.LESS_EQUAL, "<=");
+                }
+                return new Token(TokenType.LESS, "<");
+
+            case '>':
+                advance();
+                if (currentChar == '=') {
+                    advance();
+                    return new Token(TokenType.GREATER_EQUAL, ">=");
+                }
+                return new Token(TokenType.GREATER, ">");
+
+            case ';':
+                advance();
+                return new Token(TokenType.SEMICOLON, ";");
+
+            case '+':
+                advance();
+                return new Token(TokenType.PLUS, "+");
+
+            case '-':
+                advance();
+                return new Token(TokenType.MINUS, "-");
+
+            case '*':
+                advance();
+                return new Token(TokenType.MULTIPLY, "*");
+
+            case '/':
+                advance();
+                return new Token(TokenType.DIVIDE, "/");
+
+            case '%':
+                advance();
+                return new Token(TokenType.MODULO, "%");
+
+            default:
+                char unknown = currentChar;
+                advance();
+                return new Token(TokenType.UNKNOWN, String.valueOf(unknown));
+        }
+    }
+
     public List<Token> tokenize() {
-        return new ArrayList<>();
+        List<Token> tokens = new ArrayList<>();
+        Token token;
+        do {
+            token = nextToken();
+            tokens.add(token);
+        } while (token.getType() != TokenType.EOF);
+        return tokens;
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,13 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String code = "সংখ্যা বয়স = 20;";
-        Lexer lexer = new Lexer(code);
+        System.out.println("--- Test 1 ---");
+        runTest("সংখ্যা a = 10;");
 
+        System.out.println("\n--- Test 2 ---");
+        runTest("লেখা নাম = \"Mahdi\";");
+
+        System.out.println("\n--- Test 3 ---");
+        runTest("""
+        যদি (a >= 10)
+        {
+            দেখাও("OK");
+        }
+        """);
+    }
+
+    private static void runTest(String code) {
+        Lexer lexer = new Lexer(code);
         for (Token token : lexer.tokenize()) {
             System.out.println(token);
         }
-
     }
 
 }
